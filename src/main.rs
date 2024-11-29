7cadcc6d1d75cec1392f9cd862569ba3173f71f0
amdRyzen5600X <arca26100@gmail.com>
2024-11-30 01:08:33 +0500

    fix: cleaning warnings

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use ropey::Rope;
 
 fn main() -> edit_rs::app::Result<()> {
-    let mut app = edit_rs::app::App {
+    let app = edit_rs::app::App {
         file_name: "sometext.txt".to_string(),
         contents: Rope::from_reader(File::open("sometext.txt").expect("no such a file"))
             .expect("unnable to create a Rope from reader"),
